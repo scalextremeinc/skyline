afedf81c9d289f06959ee6072dec9299abc4fcc0
m <michal@duszyk.com>
2013-09-25 13:19:42 +0200

    added setup of somaxconn in uwsgi script

--- src/webapp/webapp/__init__.py
+++ src/webapp/webapp/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask, request, render_template
 from daemon import runner
 from os.path import dirname, abspath
 
+from monitorcommon import logutil
 
 # add the shared settings file's dir to python path
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
@@ -17,3 +18,5 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 # import views configures flask routes
 import webapp.views
+
+logutil.setup_logger(logging.DEBUG)
